2189333fd50ee3ed2fb11f481774f312a981c360
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-09 09:56:16 -0400

    setup basic pages

---
+++ src/app/feature/feature-2/feature-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prb-feature-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './feature-2.component.html',
+  styleUrl: './feature-2.component.scss'
+})
+export class Feature2Component {
+
+}
