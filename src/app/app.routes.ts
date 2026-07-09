f1cb340b80c9f9d4fa60c730c8fff6e01d2b7635
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-09 09:56:16 -0400

    setup basic pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./feature/home/home.component')
+      .then(m => m.HomeComponent),
+    title: 'Home'
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./feature/about/about.component')
+      .then(m => m.AboutComponent),
+    title: 'About'
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./feature/contact/contact.component')
+      .then(m => m.ContactComponent),
+    title: 'Contact Us'
+  },
+  {
+    path: 'feature-one',
+    loadComponent: () => import('./feature/feature-1/feature-1.component')
+      .then(m => m.Feature1Component),
+    title: 'Feature One'
+  },
+  {
+    path: 'feature-two',
+    loadComponent: () => import('./feature/feature-2/feature-2.component')
+      .then(m => m.Feature2Component),
+    title: 'Feature Two'
+  }
+];
