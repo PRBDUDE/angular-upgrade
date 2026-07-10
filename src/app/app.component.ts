10776f5210704c780555f80a43e6afefdb3b9de3
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-10 07:30:42 -0400

    add grid layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, HostListener, Inject, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './core/header/header.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {MenuComponent} from './core/menu/menu.component';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'prb-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-upgrade';
+export class AppComponent implements AfterViewInit {
+  title = 'Angular Upgrade';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+
+  }
+
+  contentHeight = signal<number>(0);
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.measureElements();
+    });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.measureElements();
+  }
+
+  measureElements(): void {
+    const hHeight = 64;
+    const fHeight = 34;
+    const cHeight = window.outerHeight - fHeight - hHeight - 100;
+
+    this.contentHeight.set(cHeight);
+
+    this.document.documentElement.style.setProperty('--content-height', `${cHeight}px`);
+  }
 }
