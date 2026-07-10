d95e8d7b7a83a2e35361697d23090b32fb11a701
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-10 07:28:25 -0400

    update styling of footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CopywriteComponent} from './copywrite/copywrite.component';
 
 @Component({
   selector: 'prb-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    CopywriteComponent
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
