5525eebfa1540d7f0ac9bfe8b57c94af3a3dfb6b
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-10 07:25:43 -0400

    add abstract class base-page-with-subtitle

---
+++ src/app/core/base-page-with-subtitle/base-page-with-subtitle.ts
@@ -0,0 +1,28 @@
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {HeaderService} from '../services/header.service';
+
+@Component({
+  template: ``
+})
+export abstract class BasePageWithSubtitleComponent implements OnInit, OnDestroy {
+  protected headerService = inject(HeaderService);
+  abstract readonly pageSubtitle: string;
+
+  init() {
+  }
+
+  destroy() {
+  }
+
+  ngOnInit() {
+    if (this.pageSubtitle) {
+      this.headerService.setSubtitle(this.pageSubtitle);
+    }
+    this.init();
+  }
+
+  ngOnDestroy() {
+    this.headerService.clearSubtitle();
+    this.destroy();
+  }
+}
