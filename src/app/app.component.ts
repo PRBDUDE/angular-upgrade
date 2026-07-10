00c4a542b9a6d92b6f51e16a0e4f78b543018cea
PRBDUDE <programmerroadblocks@gmail.com>
2026-07-10 10:58:13 -0400

    fix content height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import {AfterViewInit, Component, ElementRef, HostListener, Inject, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './core/header/header.component';
 import {FooterComponent} from './core/footer/footer.component';
 import {MenuComponent} from './core/menu/menu.component';
-import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'prb-root',
@@ -12,33 +11,7 @@ import {DOCUMENT} from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'Angular Upgrade';
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
-
-  }
-
-  contentHeight = signal<number>(0);
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.measureElements();
-    });
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    this.measureElements();
-  }
-
-  measureElements(): void {
-    const hHeight = 64;
-    const fHeight = 34;
-    const cHeight = window.outerHeight - fHeight - hHeight - 100;
-
-    this.contentHeight.set(cHeight);
-
-    this.document.documentElement.style.setProperty('--content-height', `${cHeight}px`);
-  }
 }
